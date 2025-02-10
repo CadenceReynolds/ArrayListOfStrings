aa5479e8b4dead2ca9331e2d0466ef9db86de78a
CadenceReynolds <reynocal001@warren.k12.in.us>
2025-02-10 13:34:01 -0500

    Finished Assignment

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
+    public static void main(String[] args) {
+        List<String> strings = new ArrayList<>();
+        strings.add("Penguin");
+        strings.add("Fox");
+        strings.add("Bunny");
+        strings.add("Wolf");
+        strings.add("Bear");
+
+        strings.remove(2);
+        System.out.println(strings);
+        Collections.sort(strings);
+        System.out.println(strings);
+
+    }
 }
